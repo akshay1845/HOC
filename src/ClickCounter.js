3e74a33a3240cb7b25a25a917b4c35e934fb7fcd
akshay1845 <akshaygadher2304@gmail.com>
2022-04-05 18:53:47 +0530

    simple hoc example

---
+++ src/ClickCounter.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import withCounter from "./withCounter";
+
+export class ClickCounter extends Component {
+
+  // constructor(props) {
+  //   super(props)
+  
+  //   this.state = {
+  //      count : 0
+  //   }
+  // }
+  
+
+  // incrementCount = ()=>{
+  //   this.setState(prevState => {
+  //     return {count :prevState.count + 1}
+  //   })
+  // }
+  render() {
+    // const { count } = this.state
+    const {count , incrementCount } = this.props
+
+    
+    return (
+     
+      <div>
+        <button onClick={incrementCount}> clicked {count} times</button>
+      </div>
+    );
+  }
+}
+
+export default withCounter(ClickCounter);
